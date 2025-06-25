8d702107a9bc77e4ba17eb994c5a4703d0fc34ca
CurateAIHealth <admin@curatehealth.in>
2025-06-25 17:11:27 +0530

    Docter Registration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,30 +14,30 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 via-white to-teal-100 px-4">
       <section className="backdrop-blur-xl bg-white/30 border border-white/40 shadow-2xl rounded-2xl p-8 max-w-md w-full text-center animate-fade-in space-y-6">
-        
-   
-        <div className="flex justify-center">
+
+
+        <div className="flex items-center justify-center ">
           <Image
             src="/Icons/Curate-logo.png"
             alt="Curate AI Health Logo"
-            width={80}
-            height={80}
-            priority 
-            className="rounded-full shadow-md transition-transform duration-300 hover:scale-110"
+            width={85}
+            height={85}
+            priority
+            className="rounded-full pl-2 shadow-md transition-transform duration-300 hover:scale-110 "
           />
         </div>
 
-        
+
         <h1 className="text-3xl md:text-4xl font-extrabold text-gray-800">
-          Register with <span className="text-teal-600">Curate Ai Health</span>
+          Register with <span className="text-teal-600">Curate Digital AI Health</span>
         </h1>
 
-       
+
         <p className="text-gray-600 text-sm md:text-base">
           Start your journey with smarter health insights.
         </p>
 
-     
+
         <button
           onClick={handleRegister}
           className="bg-teal-600 hover:bg-teal-700 text-white font-semibold px-6 py-2 rounded-full transition duration-300 shadow-lg"
