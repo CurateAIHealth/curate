bd8b990fb85640703b775407c17e030f168c03d3
CurateAIHealth <admin@curatehealth.in>
2025-07-16 17:48:40 +0530

    Changes

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -54,7 +54,6 @@ export default function RootLayout({
   return (
     <div className="flex h-screen bg-gray-100 overflow-hidden">
 
-  
       <aside
         className={`
           fixed z-30 inset-y-0 left-0 transform transition-transform duration-300 ease-in-out
@@ -105,39 +104,45 @@ export default function RootLayout({
         </div>
       </aside>
 
-     
+    
       <div className="flex flex-col flex-1 ml-0 md:ml-auto">
-
-      
+    
         <header className="bg-white shadow px-4 flex items-center justify-between" style={{ height: '10vh' }}>
-  {/* LEFT SIDE (Hamburger + Title in mobile) */}
-  <div className="flex items-center md:hidden">
-    <button
-      onClick={toggleMobileSidebar}
-      className="mr-2"
-      aria-label="Toggle Sidebar"
-    >
-      <Menu className="w-6 h-6 text-gray-700" />
-    </button>
-  </div>
+      
+          <div className="flex items-center space-x-4">
+         
+            <button
+              onClick={toggleMobileSidebar}
+              className="md:hidden"
+              aria-label="Toggle Sidebar"
+            >
+              <Menu className="w-6 h-6 text-gray-700" />
+            </button>
 
-  {/* MIDDLE (Company logo on mobile only) */}
-  <div className="md:hidden">
-    <div className="bg-white rounded-full shadow-md w-[40px] h-[40px] flex items-center justify-center ml-auto">
-      <img
-        src="/Icons/Curate-logo.png"
-        alt="Curate AI Health Logo"
-        className="w-6 h-6 object-contain"
-      />
-    </div>
-  </div>
+        
+            <div className="hidden md:flex bg-white rounded-full pl-1 shadow-md w-[40px] h-[40px] items-center justify-center">
+              <img
+                src="/Icons/Curate-logo.png"
+                alt="Curate AI Health Logo"
+                className="w-6 h-6 object-contain"
+              />
+            </div>
+          </div>
 
-  {/* RIGHT SIDE (Greeting message) */}
-  <div className="text-gray-600 hidden md:block">Welcome, {ProfileName}</div>
-</header>
-
+    
+          <div className="md:hidden bg-white rounded-full shadow-md w-[40px] h-[40px] flex items-center justify-center">
+            <img
+              src="/Icons/Curate-logo.png"
+              alt="Curate AI Health Logo"
+              className="w-6 h-6 object-contain"
+            />
+          </div>
 
        
+          <div className="text-gray-600 hidden md:block">Welcome, {ProfileName}</div>
+        </header>
+
+      
         <main className="flex-1 overflow-y-auto px-4 py-3 bg-gray-100">
           {children}
         </main>
