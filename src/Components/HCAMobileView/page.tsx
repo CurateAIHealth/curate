8240727a492437c3580001ed481bc78eb49aee56
CurateAIHealth <admin@curatehealth.in>
2025-07-21 15:33:46 +0530

    Profile Information Loading Content Updated

--- src/Components/HCAMobileView/page.tsx
+++ src/Components/HCAMobileView/page.tsx
@@ -1381,7 +1381,7 @@ export default function HCAMobileView() {
 
 
                 </div>
-                <div className="flex items-end mt-8">
+                <div className="flex justify-between items-end mt-8">
                     {currentStep > 0 && (
                         <button
                             type="button"
@@ -1406,7 +1406,7 @@ export default function HCAMobileView() {
                         <button
                             type="submit"
                             disabled={!UpdatingStatus || PictureUploading}
-                            className="px-8 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed mx-auto"
+                            className="px-6 py-2 bg-green-800 text-white rounded-lg hover:bg-green-700 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed mx-auto"
                         >
                             {UpdatingStatus ? 'Update Profile' : 'Updating...'}
                         </button>
