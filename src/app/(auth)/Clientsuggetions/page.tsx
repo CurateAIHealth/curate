9b841d7a36fa77bf4f550f43d2b4652e222783ee
CurateAIHealth <admin@curatehealth.in>
2025-10-10 17:04:10 +0530

    Changes

--- src/app/(auth)/Clientsuggetions/page.tsx
+++ src/app/(auth)/Clientsuggetions/page.tsx
@@ -1,36 +1,32 @@
-'use client'
+'use client'; 
 
-import SuitableHcpList from "@/Components/HCPSugetions/page";
-import { GetRegidterdUsers, GetUsersFullInfo } from "@/Lib/user.action";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSelector } from 'react-redux';
+import SuitableHcpList from '@/Components/HCPSugetions/page';
+import { GetRegidterdUsers, GetUsersFullInfo } from '@/Lib/user.action';
 
 const ClientSuggetions = () => {
   const [clientList, setClients] = useState([]);
   const [HCP, setHCP] = useState([]);
   const [loading, setLoading] = useState(true);
+
   const currentClientUserId = useSelector((state: any) => state.Suggested_HCP);
   const updatedRefresh = useSelector((state: any) => state.updatedCount);
   const router = useRouter();
 
   useEffect(() => {
-    const fetchData = async () => {
-      if (!currentClientUserId) {
-        router.push("/AdminPage");
-        return;
-      }
+    if (!currentClientUserId) {
+      router.push("/AdminPage");
+      return;
+    }
 
+    const fetchData = async () => {
       setLoading(true);
 
-     
-      let cachedRegisteredUsers = null;
-      let cachedFullInfo = null;
-
-      if (typeof window !== "undefined") {
-        cachedRegisteredUsers = sessionStorage.getItem("registeredUsers");
-        cachedFullInfo = sessionStorage.getItem("fullInfo");
-      }
+  
+      const cachedRegisteredUsers = sessionStorage.getItem("registeredUsers");
+      const cachedFullInfo = sessionStorage.getItem("fullInfo");
 
       const [registeredUsers, fullInfo] = await Promise.all([
         cachedRegisteredUsers
@@ -41,15 +37,13 @@ const ClientSuggetions = () => {
           : GetUsersFullInfo(),
       ]);
 
-     
-      if (typeof window !== "undefined") {
-        if (!cachedRegisteredUsers)
-          sessionStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
-        if (!cachedFullInfo)
-          sessionStorage.setItem("fullInfo", JSON.stringify(fullInfo));
-      }
+    
+      if (!cachedRegisteredUsers)
+        sessionStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
+      if (!cachedFullInfo)
+        sessionStorage.setItem("fullInfo", JSON.stringify(fullInfo));
 
-     
+   
       const filterData = registeredUsers.filter(
         (each: any) =>
           each.userType === "patient" &&
