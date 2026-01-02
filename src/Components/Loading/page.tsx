42ef02135e3c53ee96d9ee8bd63648a3de80e85d
CurateAIHealth <admin@curatehealth.in>
2026-01-02 17:58:57 +0530

    Pushed

--- src/Components/Loading/page.tsx
+++ src/Components/Loading/page.tsx
@@ -15,7 +15,6 @@ export const LoadingData=()=>{
        
           <img
             src="Icons/Curate-logoq.png"
-            onClick={()=>Router.push("/DashBoard")} 
             className="h-20 w-20 object-contain"
           />
         </div>
