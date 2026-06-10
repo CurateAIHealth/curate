c9646710264b3099f7e5c16572018f880f01782e
CurateAIHealth <admin@curatehealth.in>
2026-06-10 15:00:22 +0530

    Changes Done

--- src/Lib/db.ts
+++ src/Lib/db.ts
@@ -1,28 +1,45 @@
-
+import dns from "dns";
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://admin:KLWwnX3yKGrnPn4W@cluster0.pbueca9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Force Google DNS for SRV lookups (MongoDB Atlas)
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const uri = process.env.MONGODB_URI!;
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 declare global {
+  // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
+
 if (!uri) {
-  throw new Error("Please add your Mongo URI to .env.local");
+  throw new Error("Please define MONGODB_URI in .env.local");
 }
 
 if (process.env.NODE_ENV === "development") {
- 
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
+
+    global._mongoClientPromise = client.connect().catch((err) => {
+      console.error("MongoDB Connection Failed:", err);
+
+      // Allow reconnect attempt on next request
+      global._mongoClientPromise = undefined;
+
+      throw err;
+    });
   }
+
   clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri);
-  clientPromise = client.connect();
+
+  clientPromise = client.connect().catch((err) => {
+    console.error("MongoDB Connection Failed:", err);
+    throw err;
+  });
 }
 
 export default clientPromise;
